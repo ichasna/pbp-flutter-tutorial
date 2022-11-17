5948e6041775aa712736a86ab0f92d020604794b
syarifah <asyarifahnur@gmail.com>
2022-11-17 16:14:14 +0700

    lab8 bonus

--- lib/page/to_do_page.dart
+++ lib/page/to_do_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:aplikasicha/model/to_do.dart';
+import 'package:aplikasicha/fetch_to_do.dart';
 
 class ToDoPage extends StatefulWidget {
     const ToDoPage({Key? key}) : super(key: key);
@@ -113,30 +114,6 @@ class _ToDoPageState extends State<ToDoPage> {
         }
     )
 );
-    }
-
-    Future<List<ToDo>> fetchToDo() async {
-        var url = Uri.parse('https://jsonplaceholder.typicode.com/todos?_start=0&_limit=10');
-        var response = await http.get(
-        url,
-        headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Content-Type": "application/json",
-        },
-        );
-
-        // melakukan decode response menjadi bentuk json
-        var data = jsonDecode(utf8.decode(response.bodyBytes));
-
-        // melakukan konversi data json menjadi object ToDo
-        List<ToDo> listToDo = [];
-        for (var d in data) {
-        if (d != null) {
-            listToDo.add(ToDo.fromJson(d));
-        }
-        }
-
-        return listToDo;
     }
     
 }
